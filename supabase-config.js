0a263f8ab58ca6dced571b1ce756a17f8e7b72a5
gabrielfalleirossf-oss <gabrielfalleirossf@gmail.com>
2026-09-05 18:55:42 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // A chave "publishable"/"anon" pode ficar no site. NUNCA use a chave service_role aqui.
 window.RPG_SUPABASE = {
     url: "https://ccuuskpzsuszyaenmhni.supabase.co",
-    anonKey: "sb_publishable_JUEKfm2cV6Wg8Kfo70sgfg_CkZOYXqA"
+    anonKey: "sb_publishable_jUEKfm2cV6Wg8Kfo70sgfg_CkZOYXqA"
 };
